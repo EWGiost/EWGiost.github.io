ad02fde1c03822cee4b44a8ac6033eab1212a661
EWGiost <1205064454zzh@gmail.com>
2022-11-04 22:36:36 +0800

    yes

---
+++ DPlayer-master/docs/.vuepress/config.js
@@ -0,0 +1,78 @@
+module.exports = {
+    plugins: {
+        '@vuepress/google-analytics': {
+            ga: 'UA-48084758-9',
+        },
+        '@vuepress/back-to-top': true,
+    },
+    locales: {
+        '/zh/': {
+            lang: 'zh-CN',
+            title: 'DPlayer',
+            description: '🍭 Wow, such a lovely HTML5 danmaku video player',
+        },
+        '/': {
+            lang: 'en-US',
+            title: 'DPlayer',
+            description: '🍭 Wow, such a lovely HTML5 danmaku video player',
+        },
+    },
+    head: [
+        ['link', { rel: 'icon', href: `/logo.png` }],
+        ['script', { src: 'https://cdn.jsdelivr.net/npm/flv.js/dist/flv.min.js' }],
+        ['script', { src: 'https://cdn.jsdelivr.net/npm/hls.js/dist/hls.min.js' }],
+        ['script', { src: 'https://cdn.jsdelivr.net/npm/dashjs/dist/dash.all.min.js' }],
+        ['script', { src: 'https://cdn.jsdelivr.net/webtorrent/latest/webtorrent.min.js' }],
+        ['script', { src: 'https://cdn.jsdelivr.net/npm/dplayer/dist/DPlayer.min.js' }],
+    ],
+    theme: 'vuepress-theme-dplayer',
+    themeConfig: {
+        repo: 'MoePlayer/DPlayer',
+        editLinks: true,
+        docsDir: '.',
+        locales: {
+            '/zh/': {
+                lang: 'zh-CN',
+                selectText: '选择语言',
+                label: '简体中文',
+                editLinkText: '在 GitHub 上编辑此页',
+                lastUpdated: '上次更新',
+                nav: [
+                    {
+                        text: '指南',
+                        link: '/zh/guide/',
+                    },
+                    {
+                        text: '生态',
+                        link: '/zh/ecosystem/',
+                    },
+                    {
+                        text: '支持 DPlayer',
+                        link: '/zh/support/',
+                    },
+                ],
+            },
+            '/': {
+                lang: 'en-US',
+                selectText: 'Languages',
+                label: 'English',
+                editLinkText: 'Edit this page on GitHub',
+                lastUpdated: 'Last Updated',
+                nav: [
+                    {
+                        text: 'Guide',
+                        link: '/guide/',
+                    },
+                    {
+                        text: 'Ecosystem',
+                        link: '/ecosystem/',
+                    },
+                    {
+                        text: 'Support DPlayer',
+                        link: '/support/',
+                    },
+                ],
+            },
+        },
+    },
+};
---
+++ DPlayer-master/webpack/prod.config.js
@@ -0,0 +1,99 @@
+const path = require('path');
+const webpack = require('webpack');
+const { GitRevisionPlugin } = require('git-revision-webpack-plugin');
+const gitRevisionPlugin = new GitRevisionPlugin();
+
+module.exports = {
+    mode: 'production',
+
+    bail: true,
+
+    devtool: 'source-map',
+
+    entry: {
+        DPlayer: './src/js/index.js',
+    },
+
+    output: {
+        path: path.resolve(__dirname, '..', 'dist'),
+        filename: '[name].min.js',
+        library: '[name]',
+        libraryTarget: 'umd',
+        libraryExport: 'default',
+        umdNamedDefine: true,
+        publicPath: '/',
+    },
+
+    resolve: {
+        modules: ['node_modules'],
+        extensions: ['.js', '.less'],
+        fallback: {
+            dgram: false,
+            fs: false,
+            net: false,
+            tls: false,
+        },
+    },
+
+    module: {
+        strictExportPresence: true,
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    'template-string-optimize-loader',
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            cacheDirectory: true,
+                            presets: ['@babel/preset-env'],
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                        },
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: ['postcss-preset-env'],
+                            },
+                        },
+                    },
+                    'less-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 40000,
+                },
+            },
+            {
+                test: /\.svg$/,
+                loader: 'svg-inline-loader',
+            },
+            {
+                test: /\.art$/,
+                loader: 'art-template-loader',
+            },
+        ],
+    },
+
+    plugins: [
+        new webpack.DefinePlugin({
+            DPLAYER_VERSION: `"${require('../package.json').version}"`,
+            GIT_HASH: JSON.stringify(gitRevisionPlugin.version()),
+        }),
+    ],
+};
